2b5a8be812df6505ab22aebfb65118257fc5fe70
Abhilashd8 <133367221+Abhilashd8@users.noreply.github.com>
2025-04-08 17:46:58 +0530

    Update streamlit_app.py
    my_dataframe should be after the connection parameter

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ from snowflake.snowpark.functions import col
 st.title(":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
 
 st.write(" Choose the fruits you want in your custom smoothie!")
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 name_on_order = st.text_input('Name on smoothie: ')
@@ -22,6 +22,8 @@ st.write('The name on your smoothie will be: ',name_on_order)
 cnx = st.connection("snowflake")
 
 session = cnx.session()
+
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 
 ingrediants_list = st.multiselect('Choose upto 5 ingrediants'
