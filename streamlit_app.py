614e785b21dd0a455f2bb1875647fd1e780e7918
Abhilashd8 <133367221+Abhilashd8@users.noreply.github.com>
2025-04-08 17:41:58 +0530

    Update streamlit_app.py
    Changes the position of the dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,16 +12,17 @@ from snowflake.snowpark.functions import col
 st.title(":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
 
 st.write(" Choose the fruits you want in your custom smoothie!")
-
-cnx = st.connection("snowflake")
-
-session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 name_on_order = st.text_input('Name on smoothie: ')
 
 st.write('The name on your smoothie will be: ',name_on_order)
+
+cnx = st.connection("snowflake")
+
+session = cnx.session()
+
 
 ingrediants_list = st.multiselect('Choose upto 5 ingrediants'
                                   , my_dataframe
